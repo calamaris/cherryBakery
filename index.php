7f498048f8fb2e5f66cb229cf2fb58b1847d0950
Oscar Crespo <crespo.krw@gmail.com>
2015-09-19 21:02:57 -0500

    Header

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cherry Bakery</title>
+	<link rel="stylesheet" href="shared/css/main.css">
+</head>
+<body>
+	<header>
+		<h1><a href="index.php"><img src="shared/img/cherryLogo.png" width="100%"  alt="Cherry Bakery logo"></a><span>Cherry Bakery</span><strong>MATERIAS PRIMAS</strong></h1>
+		<nav>
+			<div class="redes">
+				<a href="#" target="_blank"><div class="redIcon facebook"></div></a>
+				<a href="#" target="_blank"><div class="redIcon twitter"></div></a>
+				<a href="#" target="_blank"><div class="redIcon gplus"></div></a>
+				<a href="#" target="_blank"><div class="redIcon youtube"></div></a>
+			</div>
+			<ul class="nav">
+				<li><a href="#.php">NOSOTROS</a></li>
+				<li><a href="#.php">PRODUCTOS</a></li>
+				<li><a href="#.php">CURSOS</a></li>
+				<li><a href="#.php">VIDEORECETAS</a></li>
+				<li><a href="#.php">CONTACTO</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div id="plecaRoja"></div>
+	<main>
+		
+	</main>
+</body>
+</html>
