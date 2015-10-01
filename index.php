24f451b491491551ec8359ad20042144d542b8d1
Oscar Crespo <crespo.krw@gmail.com>
2015-10-01 01:25:42 -0500

    Menu y submenu

--- index.php
+++ index.php
@@ -16,11 +16,35 @@
 				<a href="#" target="_blank"><div class="redIcon youtube"></div></a>
 			</div>
 			<ul class="nav">
-				<li><a href="#.php">NOSOTROS</a></li>
-				<li><a href="#.php">PRODUCTOS</a></li>
-				<li><a href="#.php">CURSOS</a></li>
-				<li><a href="#.php">VIDEORECETAS</a></li>
-				<li><a href="#.php">CONTACTO</a></li>
+				<li class="menuItem"><a href="#.php">NOSOTROS</a></li>
+				<li class="sm-trigger menuItem"><a href="#.php">PRODUCTOS</a>
+					<div class="submenu">
+						<ul class="sm-col list barRight">
+							<li><a href="#">Margarinas</a></li>
+							<li><a href="#">Mermeladas</a></li>
+							<li><a href="#">Coberturas</a></li>
+							<li><a href="#">Polvos para Hornear</a></li>
+							<li><a href="#">Mejorantes</a></li>
+							<li><a href="#">Escencias</a></li>
+							<li><a href="#">Grenetinas</a></li>
+						</ul>
+						<ul class="sm-col list">
+							<li><a href="#">Margarinas</a></li>
+							<li><a href="#">Mermeladas</a></li>
+							<li><a href="#">Coberturas</a></li>
+							<li><a href="#">Polvos para Hornear</a></li>
+							<li><a href="#">Mejorantes</a></li>
+							<li><a href="#">Escencias</a></li>
+							<li><a href="#">Grenetinas</a></li>
+						</ul>
+						<ul class="sm-col">
+							<img id="imagenSubmenu" width="100%" src="shared/img/imagenMargarina.jpg" alt="">
+						</ul>
+					</div>	
+				</li>
+				<li class="menuItem"><a href="#.php">CURSOS</a></li>
+				<li class="menuItem"><a href="#.php">VIDEORECETAS</a></li>
+				<li class="menuItem"><a href="#.php">CONTACTO</a></li>
 			</ul>
 		</nav>
 	</header>
