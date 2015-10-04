49dd4c90423efb6987114544dc4df4273a0ad6f2
Oscar Crespo <crespo.krw@gmail.com>
2015-10-03 21:00:18 -0500

    Home 90%

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
 	<meta charset="UTF-8">
 	<title>Cherry Bakery</title>
 	<link rel="stylesheet" href="shared/css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="shared/img/slider/themes/default/default.css" type="text/css" />
+	<link rel="stylesheet" href="shared/css/nivo-slider.css">
 </head>
 <body>
 	<header>
@@ -50,7 +54,33 @@
 	</header>
 	<div id="plecaRoja"></div>
 	<main>
+		<div class="slider-wrapper theme-default">
+			<div id="slider" class="nivoSlider">
+				<img src="shared/img/slider/slideEX.jpg"  alt="">
+				<img src="shared/img/slider/slide2.jpg"  alt="">
+			</div>
+		</div>
+		
+		<div class="cursosZone">
+			<div class="btnClientesSC" id="btn-Cursos">
+				<h2>CURSOS PRACTIVOS
+				<i class="fa fa-plus iconMas"></i></h2>
+				
+			</div>
+			<div class="btnClientesSC" id="btn-Demos">
+				<h2>DEMOSTRACIONES
+				<i class="fa fa-plus iconMas"></i>
+				</h2>
+				
+			</div>
+		</div>
 		
 	</main>
+	<footer>
+		
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="shared/js/jquery.nivo.slider.js"></script>
+	<script type="text/javascript" src="shared/js/main.js"></script>
 </body>
 </html>
