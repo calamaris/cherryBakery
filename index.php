41406f6f61ba73573bd8bf6383d7158739ebe988
Oscar Crespo <crespo.krw@gmail.com>
2015-10-05 02:12:53 -0500

    home 90%

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 			</div>
 		</div>
 		
-		<div class="cursosZone">
+		<section class="cursosZone">
 			<div class="btnClientesSC" id="btn-Cursos">
 				<h2>CURSOS PRACTIVOS
 				<i class="fa fa-plus iconMas"></i></h2>
@@ -73,11 +73,58 @@
 				</h2>
 				
 			</div>
-		</div>
-		
+		</section>
+		<section class="bannersZone">
+			<img src="shared/img/bannerA.jpg" alt="Cherry Bakery" width="100%">
+			<img src="shared/img/bannerB.jpg" alt="Cherry Bakery" width="100%">
+		</section>
 	</main>
+	<aside class="subscribeZone">
+		<div class="iw">
+			<div class="copy">
+				<i class="fa fa-envelope d_ib cartaHome"></i>
+				<p class="d_ib">
+					SUSCRIBETE A NUESTRO BOLETÍN para recibir noticias, <br> promociones y muchas cosas más. 
+				</p> 
+			</div>
+			<div class="inputZone">
+				<form action="">
+					<input class="newsMail" name="newsCorreo" placeholder="INGRESA TU EMAIL" type="text">
+					<!-- <input type="submit" placeholder="SUSCRIBIRSE"> -->
+					<button class="newsButton" type="button" return false>SUSCRIBIRSE</button>
+				</form>
+			</div>
+		</div>
+	</aside>
 	<footer>
-		
+		<div class="iw">	
+			<div class="leftCol">
+				<img src="shared/img/cherryLogo.png" class="logoFooter" alt="Cherry Bakery logo">
+				<div class="sucursalWrapper">
+					<h6>CHERRY BAKERY SUCURSAL NEZA</h6>
+					<P class="dir">
+						Av. Pantitlan No. 156, Col. México 2, Nezahualcoyotl Edo. de México. C.P.57620.
+					</P>
+					<p  class="tels">
+						Tels.: 5765-4370 / 2232-8424 / 5765-4372 / 2619-9715
+					</p>
+				</div>
+				<div class="sucursalWrapper">
+					<h6>CHERRY BAKERY SUCURSAL CENTRO</h6>
+					<P class="dir">
+						Calle Americas No.118, Colonia Moderna Delegación Benito Juarez, CP. 03510					</P>
+					<p  class="tels">
+						Tels.: 5590-9865 / 5590-9297 / 5590-9318 
+					</p>
+				</div>
+
+			</div>
+			<div class="rightCol">
+				
+			</div>
+		</div>	
+		<p class="legales top">Todos los Derechos Reservados a www.cherrybakery.com 2012 Cherry Bakery ®</p>
+		<p class="legales">El contenido en este sitio es de uso exclusivo del mismo y no puede ser manipulado ni utilizado sin la autorización de su registrante. "Conoce el Aviso de Privacidad"</p>
 	</footer>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script type="text/javascript" src="shared/js/jquery.nivo.slider.js"></script>
