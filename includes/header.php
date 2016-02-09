cf0f0b2609df39dc51cd981c1adb198540b10332
EmaVizard <ema_gohan@hotmail.com>
2016-02-09 10:41:11 -0600

    cambios al 2/9/16

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<header>
+		<h1><a href="index.php"><img src="shared/img/cherryLogo.png" width="100%"  alt="Cherry Bakery logo"></a><span>Cherry Bakery</span><strong>MATERIAS PRIMAS</strong></h1>
+		<nav>
+			<div class="redes">
+				<a href="#" target="_blank"><div class="redIcon facebook"></div></a>
+				<a href="#" target="_blank"><div class="redIcon twitter"></div></a>
+				<a href="#" target="_blank"><div class="redIcon gplus"></div></a>
+				<a href="#" target="_blank"><div class="redIcon youtube"></div></a>
+			</div>
+			<ul class="nav">
+				<li class="menuItem"><a href="#.php">NOSOTROS</a></li>
+				<li class="sm-trigger menuItem"><a href="#.php">PRODUCTOS</a>
+					<div class="submenu">
+						<ul class="sm-col list barRight">
+							<li><a href="#">Margarinas</a></li>
+							<li><a href="#">Mermeladas</a></li>
+							<li><a href="#">Coberturas</a></li>
+							<li><a href="#">Polvos para Hornear</a></li>
+							<li><a href="#">Mejorantes</a></li>
+							<li><a href="#">Escencias</a></li>
+							<li><a href="#">Grenetinas</a></li>
+						</ul>
+						<ul class="sm-col list">
+							<li><a href="#">Margarinas</a></li>
+							<li><a href="#">Mermeladas</a></li>
+							<li><a href="#">Coberturas</a></li>
+							<li><a href="#">Polvos para Hornear</a></li>
+							<li><a href="#">Mejorantes</a></li>
+							<li><a href="#">Escencias</a></li>
+							<li><a href="#">Grenetinas</a></li>
+						</ul>
+						<ul class="sm-col">
+							<img id="imagenSubmenu" width="100%" src="shared/img/imagenMargarina.jpg" alt="">
+						</ul>
+					</div>	
+				</li>
+				<li class="menuItem"><a href="#.php">CURSOS</a></li>
+				<li class="menuItem"><a href="#.php">VIDEORECETAS</a></li>
+				<li class="menuItem"><a href="#.php">CONTACTO</a></li>
+			</ul>
+		</nav>
+	</header>
--- index.php
+++ index.php
@@ -5,53 +5,13 @@
 	<title>Cherry Bakery</title>
 	<link rel="stylesheet" href="shared/css/main.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
 	<link rel="stylesheet" href="shared/img/slider/themes/default/default.css" type="text/css" />
 	<link rel="stylesheet" href="shared/css/nivo-slider.css">
 </head>
 <body>
-	<header>
-		<h1><a href="index.php"><img src="shared/img/cherryLogo.png" width="100%"  alt="Cherry Bakery logo"></a><span>Cherry Bakery</span><strong>MATERIAS PRIMAS</strong></h1>
-		<nav>
-			<div class="redes">
-				<a href="#" target="_blank"><div class="redIcon facebook"></div></a>
-				<a href="#" target="_blank"><div class="redIcon twitter"></div></a>
-				<a href="#" target="_blank"><div class="redIcon gplus"></div></a>
-				<a href="#" target="_blank"><div class="redIcon youtube"></div></a>
-			</div>
-			<ul class="nav">
-				<li class="menuItem"><a href="#.php">NOSOTROS</a></li>
-				<li class="sm-trigger menuItem"><a href="#.php">PRODUCTOS</a>
-					<div class="submenu">
-						<ul class="sm-col list barRight">
-							<li><a href="#">Margarinas</a></li>
-							<li><a href="#">Mermeladas</a></li>
-							<li><a href="#">Coberturas</a></li>
-							<li><a href="#">Polvos para Hornear</a></li>
-							<li><a href="#">Mejorantes</a></li>
-							<li><a href="#">Escencias</a></li>
-							<li><a href="#">Grenetinas</a></li>
-						</ul>
-						<ul class="sm-col list">
-							<li><a href="#">Margarinas</a></li>
-							<li><a href="#">Mermeladas</a></li>
-							<li><a href="#">Coberturas</a></li>
-							<li><a href="#">Polvos para Hornear</a></li>
-							<li><a href="#">Mejorantes</a></li>
-							<li><a href="#">Escencias</a></li>
-							<li><a href="#">Grenetinas</a></li>
-						</ul>
-						<ul class="sm-col">
-							<img id="imagenSubmenu" width="100%" src="shared/img/imagenMargarina.jpg" alt="">
-						</ul>
-					</div>	
-				</li>
-				<li class="menuItem"><a href="#.php">CURSOS</a></li>
-				<li class="menuItem"><a href="#.php">VIDEORECETAS</a></li>
-				<li class="menuItem"><a href="#.php">CONTACTO</a></li>
-			</ul>
-		</nav>
-	</header>
+
+	<?php include("includes/header.php"); ?>
+
 	<div id="plecaRoja"></div>
 	<main>
 		<div class="slider-wrapper theme-default">
